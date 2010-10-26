756bb036e5ea0458efe9ed1f99169c99bd457a00
Manel <manuel.roma01@gmail.com>
2010-10-26 23:56:27 +0200

    Ejercicios cap 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
   get "pages/home" # redirige la url /pages/home a :controller => pages, :action => home
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
