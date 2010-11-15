2a0f124d405319a026873c9847f912a36295f87c
Manel <manuel.roma01@gmail.com>
2010-11-15 23:26:50 +0100

    Sign in terminado

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,6 +35,11 @@ Spork.prefork do
     # Emulate initializer set_clear_dependencies_hook in 
     # railties/lib/rails/application/bootstrap.rb
     ActiveSupport::Dependencies.clear
+    
+    # tests con usuario logeado
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
   end
 end
 
